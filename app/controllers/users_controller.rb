6c671e64ad532b428652f536bac3df9827eb6dd7
Yuta Ushijima <register@yuta-u.com>
2019-03-30 23:31:07 +0900

    Add action in users_cotroller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,31 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :update, :destroy]
   def index
-    @users = User.order("created_at DESC")
+    @users = User.all
+  end
+
+  def create
+    User.create!(user_params)
   end
 
   def show
-    @user = User.find(params[:id])
   end
+
+  def update
+    @user.update!(user_params)
+  end
+
+  def destroy
+    @user.destroy!
+  end
+
+  private
+
+    def set_user
+      @user = User.find(params[:id])
+    end
+
+    def user_params
+      params.require(:user).permit(:account, :name, :email)
+    end
 end
