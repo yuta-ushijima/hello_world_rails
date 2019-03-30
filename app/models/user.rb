433c34595d0fa672abc46ce0f01935083588aaf8
Yuta Ushijima <register@yuta-u.com>
2019-03-30 23:39:47 +0900

    Create index of user_id in articles table

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,7 @@ class User < ApplicationRecord
   # validations
   validates :account, presence: true
   validates :account, uniqueness: true
+
+  # association
+  has_many :articles
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_30_111158) do
+ActiveRecord::Schema.define(version: 2019_03_30_143324) do
 
   create_table "articles", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "title"
     t.string "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_articles_on_user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -27,4 +29,5 @@ ActiveRecord::Schema.define(version: 2019_03_30_111158) do
     t.string "email"
   end
 
+  add_foreign_key "articles", "users"
 end
