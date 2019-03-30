f9931440e00723a1e1fe7fe18c786d6272e65174
Yuta Ushijima <register@yuta-u.com>
2019-03-30 21:01:28 +0900

    Create show action of users_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,9 @@
 class UsersController < ApplicationController
   def index
-    @users = User.all
-    render json: @users
+    @users = User.order("created_at DESC")
+  end
+
+  def show
+    @user = User.find(params[:id])
   end
 end
