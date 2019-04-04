796aa4e10654b622193eaddb5a206c71a4155f91
Yuta Ushijima <register@yuta-u.com>
2019-04-04 23:45:39 +0900

    Create users_spec template

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -2,9 +2,31 @@ require 'rails_helper'
 
 RSpec.describe "Users", type: :request do
   describe "GET /users" do
-    it "works! (now write some real specs)" do
-      get users_path
-      expect(response).to have_http_status(200)
+    it "ユーザー一覧が拾得できること" do
+    end
+  end
+
+  describe "POST /users" do
+    it "ユーザーのレコードが作成できること" do
+
+    end
+  end
+
+  describe "GET /users/:id" do
+    it '任意のユーザーの値が取得できること' do
+
+    end
+  end
+
+  describe "PATCH /users/:id" do
+    it '任意のユーザーのレコードが更新できること' do
+
+    end
+  end
+
+  describe "DELETE /users/:id" do
+    it '任意のユーザーのレコードが削除できること' do
+
     end
   end
 end
