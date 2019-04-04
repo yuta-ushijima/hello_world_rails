523733368d845d7782d004cd10894af70612d13d
Yuta Ushijima <register@yuta-u.com>
2019-04-04 23:26:19 +0900

    Refactor user_spec.rb

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,23 +3,26 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   describe "validations" do
     context "accountを指定しているとき" do
-      let(:user) { User.new(account: "marip", name: "マリオ", email: "mario@example.com") }
+      let(:user) { build(:user) }
+
       it 'ユーザーが作成できること' do
         expect(user).to be_valid
       end
     end
 
     context "accountを指定していないとき" do
-      let(:user) { User.new(account: nil, name: "マリオ", email: "mario@example.com") }
+      let(:user) { build(:user, account: nil) }
+
       it 'バリデーションエラーが返ること' do
         user.valid?
         expect(user.errors.messages[:account]).to include("can't be blank")
       end
     end
 
-    context "accnountが重複しているとき" do
+    context "accountが重複しているとき" do
       let(:user) { create(:user) }
-      let(:new_user) { User.new(account: user.account, name: "luige", email: "luige@example.com")}
+      let(:new_user) { build(:user, account: user.account)}
+
       it 'バリデーションエラーとなること' do
         new_user.valid?
         expect(new_user.errors.messages[:account]).to include("has already been taken")
