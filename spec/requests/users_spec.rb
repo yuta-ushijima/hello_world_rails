2c1ced5c509595311f948b90ebac44e88b1c604e
Yuta Ushijima <register@yuta-u.com>
2019-04-05 00:36:08 +0900

    Create index request spec

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -2,7 +2,18 @@ require 'rails_helper'
 
 RSpec.describe "Users", type: :request do
   describe "GET /users" do
-    it "ユーザー一覧が拾得できること" do
+    subject { get(users_path) }
+
+    before do
+      create_list(:user, 3)
+    end
+    it "ユーザー一覧が取得できること" do
+      subject
+      res = JSON.parse(response.body)
+      expect(res.length).to eq 3
+      expect(res[0].keys).to eq %w(account name email)
+      expect(response.status).to eq 200
+      # 別解: expect(response).to have_http_status(200)
     end
   end
 
