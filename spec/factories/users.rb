433c03c08dba124d280eda6ada14583531ebb997
Yuta Ushijima <register@yuta-u.com>
2019-04-05 22:04:18 +0900

    Sequenserize of account and email

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,9 @@
 FactoryBot.define do
   factory :user do
-    account { Faker::Internet.user_name }
-    email { Faker::Internet.email }
+    _account = Faker::Internet.username
+
+    sequence(:account) { |n| "#{n}_#{_account}" }
+    sequence(:email) { |n| Faker::Internet.email("#{n}_#{_account}") }
     name { Faker::Games::SuperSmashBros.fighter }
   end
 end
