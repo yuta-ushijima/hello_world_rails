edd0d99ac37103490a0176570b94a7562c961ae8
Yuta Ushijima <register@yuta-u.com>
2019-04-05 22:16:39 +0900

    Using trail of user_spec

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -5,5 +5,10 @@ FactoryBot.define do
     sequence(:account) { |n| "#{n}_#{_account}" }
     sequence(:email) { |n| Faker::Internet.email("#{n}_#{_account}") }
     name { Faker::Games::SuperSmashBros.fighter }
+
+    trait :with_user_detail do
+      # association :user_detail, factory: :user_detailと同義
+      user_detail
+    end
   end
 end
