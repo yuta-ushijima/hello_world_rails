02119e30d21bed1a84db7490f18df85a153a273b
Yuta Ushijima <register@yuta-u.com>
2019-04-09 19:01:00 +0900

    Create models/article_spec.rb

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Article, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe "Associations" do
+    context "articleが投稿されたとき" do
+      let(:user) { create(:user)}
+      let(:article) { create(:article, user_id: user.id)}
+      it "関連するuserのレコードが取得できること" do
+        expect(article.user_id).to eq(user.id)
+      end
+    end
+  end
 end
