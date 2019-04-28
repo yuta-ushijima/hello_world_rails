d79f393622ae9b32297e1495f33767330db8c5dd
Yuta Ushijima <register@yuta-u.com>
2019-04-28 19:43:42 +0900

    Modified each files for Hello vue

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -2,6 +2,5 @@ class HomesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index]
 
   def index
-    @message = "Hello Rails!"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
+  root "homes#index"
   resources :articles
   resources :users, defaults: { format: :json }
-  get "homes/index"
-  # get 'users/index'
 end
