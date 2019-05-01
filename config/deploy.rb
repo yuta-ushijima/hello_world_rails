2590b8bfe5aeef136aa56b35b9b09968ffbb9fcf
Yuta Ushijima <register@yuta-u.com>
2019-05-01 17:38:40 +0900

    Modified config files for webpack

--- config/deploy.rb
+++ config/deploy.rb
@@ -32,7 +32,7 @@ set :deploy_to, "/var/www/#{fetch(:application)}"
 set :linked_files, %w[config/database.yml config/master.key]
 
 # Default value for linked_dirs is []
-append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
+append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/packs", ".bundle", "node_modules"
 
 set :rbenv_type, :user
 set :rbenv_ruby, "2.6.1"
@@ -52,3 +52,16 @@ set :keep_releases, 3
 # Uncomment the following to require manually verifying the host key before first deploy.
 # set :ssh_options, verify_host_key: :secure
 set :ssh_options, keepalive: true, keepalive_interval: 60
+
+# INFO: https://github.com/rails/webpacker/blob/3d67f9c1b1c9054ec4f142947a50c1d965f279dc/docs/deployment.md#capistrano
+before "deploy:assets:precompile", "deploy:yarn_install"
+namespace :deploy do
+  desc "Run rake yarn install"
+  task :yarn_install do
+    on roles(:web) do
+      within release_path do
+        execute("cd #{release_path} && yarn install --silent --no-progress --no-audit --no-optional")
+      end
+    end
+  end
+end
